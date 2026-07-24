8d9bd7b05333a2b94a69357ef6448a4dfdf3b052
Aditya Kumar <aditya.kumar@westfieldspecialty.net>
2026-07-24 10:53:18 +0100

    Setup ValueSource & MethodSource

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -7,8 +7,13 @@ import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.annotations.Steps;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 public class GoogleSearchTest{
@@ -22,5 +27,31 @@ public class GoogleSearchTest{
     @Test
     public void searchGoogle(){
         googleSteps.searchFor("Selenium with Serenity");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "Selenium",
+        "Serenity BDD",
+        "Playwright",
+        "JUnit 5"
+    })
+    public void searchGoogleWithValueSource(String searchText){
+        googleSteps.searchFor(searchText);
+    }
+
+    @ParameterizedTest
+    @MethodSource("searchData")
+    void searchGoogleWithMethodSource(String searchText) {
+        googleSteps.searchFor(searchText);
+    }
+
+    static Stream<String> searchData() {
+        return Stream.of(
+        "Selenium",
+        "Serenity BDD",
+        "Playwright",
+        "Java"
+        );
     }
 }
